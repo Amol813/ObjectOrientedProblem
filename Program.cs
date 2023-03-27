7e2c9f479bb4cf72cf8a7177c5b557c184faa781
unknown <amoljadhav>
2023-03-27 14:27:54 +0530

    [Amol]Add:OOpsProblem

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OOPsProblemStatement.InventoryManagement;
+using System;
+
+namespace OOPsProblemStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            InventoryOperation inventory = new InventoryOperation();
+            inventory.ReadJsonFile(@"E:\pl_.net_exercise\OOPsProblemStatement1\InventoryManagementProblem\inventory.json");
+
+            InventoryDataManagement dataManagement = new InventoryDataManagement();
+            dataManagement.ReadJsonFile(@"E:\pl_.net_exercise\OOPsProblemStatement1\InventoryManagement\InventoryDetail.json");
+        }
+    }
+}
